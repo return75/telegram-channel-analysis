275c02e3f34fb1b8bbea895a8d15d3d4f68c2ae2
Dev5 <dev5@webteamwork.ca>
2025-08-26 23:46:02 +0430

    handle files

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const prompt = PromptSync();
 const apiId = 27551667;
 const apiHash = "fb00dcbf80562d3444537f792e3762af";
 
-let run = async () => {
+const run = async () => {
     const sessionString = fs.readFileSync("session.txt", "utf-8")
     const session = sessionString ? new StringSession(sessionString) : new StringSession("")
     let client = new TelegramClient(session, apiId, apiHash, {
@@ -19,11 +19,11 @@ let run = async () => {
     listenToChats(client, ['@CloudHesoyam', '@soloAnalyze'])
 }
 
-let startClient = async (client) => {
+const startClient = async (client) => {
     await client.start({
-        phoneNumber: async () => prompt("شماره‌تو وارد کن: "),
-        password: async () => prompt("پسورد 2FA (اگه داری): "),
-        phoneCode: async () => prompt("کدی که تلگرام فرستاده: "),
+        phoneNumber: async () => prompt("Enter your phone number in international format: (like +989154326509): "),
+        password: async () => prompt("Enter your 2FA password: "),
+        phoneCode: async () => prompt("Enter telegram code: "),
         onError: (err) => console.log(err),
     });
     const sessionString = client.session.save();
@@ -31,11 +31,40 @@ let startClient = async (client) => {
     console.log("Session Started");
 }
 
-let listenToChats = (client, chats) => {
+const listenToChats = (client, chats) => {
     client.addEventHandler(async (event) => {
         const message = event.message;
-        console.log("new Message:", message.text);
+        handleMedia(message.media, client)
+       // console.log("event: ", event);
     }, new NewMessage({chats}));
+}
+
+const handleMedia = (media, client) => {
+    if (!media) return
+    handleMediaDocument(media, client)
+}
+
+const handleMediaDocument = async (media, client) => {
+    if (!media.document) return
+
+    const fileNameAttr = media.document.attributes.find(attr => attr.className === "DocumentAttributeFilename");
+    const fileName = fileNameAttr ? fileNameAttr.fileName : "unknown";
+    const extension = fileName.split(".").pop();
+    console.log("fileName & extension:", fileName , extension);
+
+    if (extension === 'txt') {
+         const buffer = await client.downloadMedia(media, { workers: 1 });
+         handleTextBuffer(buffer)
+    }
+}
+
+const handleTextBuffer = buffer => {
+    const text = buffer.toString("utf-8");
+    analyzeText(text)
+}
+
+const analyzeText = text => {
+
 }
 
 run()
